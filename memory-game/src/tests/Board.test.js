efd87f39e57f751dd29fd2d81ab1a140d3e4d54c
TshepoMohlophehi <mohlophehitshepo@gmail.com>
2025-04-13 22:24:32 +0200

    remove files

--- memory-game/src/tests/Board.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Board from "../components/Board";
-
-test("renders memory game board", () => {
-  render(<Board />);
-  const boardElement = screen.getByTestId("game-board");
-  expect(boardElement).toBeInTheDocument();
-});
