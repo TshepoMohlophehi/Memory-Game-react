c8096b758ce6084b2cc256f2c94baa247dbf4d62
TshepoMohlophehi <mohlophehitshepo@gmail.com>
2025-05-28 22:26:48 +0200

    updates

--- src/components/ReactionGif.jsx
+++ src/components/ReactionGif.jsx
@@ -1,38 +1,59 @@
-import waitingGif from '../assets/waitReaction.gif';
-import matchGif from '../assets/winReaction.gif';
-import noMatchGif from '../assets/loseReaction.gif';
+import waitingGif from "../assets/waitReaction.gif";
+import matchGif from "../assets/winReaction.gif";
+import noMatchGif from "../assets/loseReaction.gif";
 
 const feedbackMap = {
-    waiting: {
-        gif: waitingGif,
-        message: "Waiting for your move..."
-      },
-      match: {
-        gif: matchGif,
-        message: "🎉🎉🎉🎉🎉🎉 Matched!"
-      },
-      'no-match': {
-        gif: noMatchGif,
-        message: "😬 Not Matched!"
-      },
-      win: {
-        gif: matchGif,
-        message: "🎉 Congratulations! You matched all the fruits!"
-      },
+  waiting: {
+    gif: waitingGif,
+    message: "Waiting for your move...",
+  },
+  match: {
+    gif: matchGif,
+    message: "🎉🎉🎉🎉🎉🎉 Matched!",
+  },
+  "no-match": {
+    gif: noMatchGif,
+    message: "😬 Not Matched!",
+  },
+  win: {
+    gif: matchGif,
+    message: "🎉 Congratulations! You matched all the fruits!",
+  },
 };
 
 export default function ReactionGif({ gameState }) {
-    const feedback = feedbackMap[gameState]
+  const feedback = feedbackMap[gameState];
   return (
-    <div style={{ width: '250px', height: '250px', marginTop: '50px', position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+    <div
+      style={{
+        width: "250px",
+        height: "250px",
+        marginTop: "50px",
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <img
         src={feedback.gif}
         alt={gameState}
-        style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        style={{ width: "100%", height: "100%", objectFit: "cover" }}
       />
 
-      <h3 style={feedback.message === "Waiting for your move..." ? { marginTop: '10px', color: 'black', whiteSpace: 'nowrap'} : 
-      feedback.message === "😬 Not Matched!" ? { marginTop: '10px', color: 'red', whiteSpace: 'nowrap'} : { marginTop: '10px', color: 'green', textAlign:"center"}}> {feedback.message} </h3>
+      <h3
+        style={
+          feedback.message === "Waiting for your move..."
+            ? { marginTop: "10px", color: "black", whiteSpace: "nowrap" }
+            : feedback.message === "😬 Not Matched!"
+            ? { marginTop: "10px", color: "red", whiteSpace: "nowrap" }
+            : { marginTop: "10px", color: "green", textAlign: "center" }
+        }
+      >
+        {" "}
+        {feedback.message}{" "}
+      </h3>
     </div>
   );
 }
